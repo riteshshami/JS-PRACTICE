a21a2c7cd331a8c05a00d61036114c55e8379db0
Ritesh <96076485+riteshshami@users.noreply.github.com>
2024-04-18 06:30:18 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,28 @@
+"use strict"; //treat all JS code as newer version
+
+// alert("hello") //we are using node.js not browser
+
+console.log(3
++ 3) // code readability should be high
+
+console.log("Ritesh")
+
+let name = "Ritesh"; //string
+let age = 18; //number
+let isLoggedIn = false; //boolean
+let state;
+
+// number => 2 to power 53
+// bigInt
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>  
+// symbol => unique
+
+
+// object
+
+console.log(typeof null);
+console.log(typeof undefined);
+
