03ed1cbd2910da48c9b7263616c38a57ffd711bc
Ritesh <96076485+riteshshami@users.noreply.github.com>
2024-04-18 07:22:18 +0000

    Datatype conversion

--- 01_basics/03_conversionsOperations.js
+++ 01_basics/03_conversionsOperations.js
@@ -23,5 +23,34 @@ let booleanIsLoggedIn = Boolean(isLoggedIn);
 
 let sumNumber = 33;
 
-let stringNumber = String(sumNumber);
-console.log(stringNumber);
+let stringNumber = Number(isLoggedIn);
+// console.log(stringNumber);
+
+// *************************************************************** Operations ***************************************************************
+
+let value = 3;
+let negValue = -value;
+// console.log(negValue);
+
+let str1 = "hello";
+let str2 = " ritesh";
+
+let str3 = str1 + str2;
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
+
