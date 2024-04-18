50f66b0240a79568689b84ab382a0149d7ae9cc3
Ritesh <riteshshami@gmail.com>
2024-04-18 16:24:48 +0530

    strings method

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,36 @@
+const name = 'ritesh';
+const repoCount = 50;
+
+console.log(name + repoCount + " Value");
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+// declaration of string
+const gameName = new String('hiteshh-hc-youtube-javascript')
+
+console.log(gameName[0]);
+console.log(gameName.__proto__);
+
+console.log(gameName.toUpperCase());
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('t'));
+
+const newString = gameName.substring(2, 5);
+
+console.log(newString);
+
+// slice mdn
+const anotherString = gameName.slice(-7, 5);
+console.log(anotherString);
+
+const newString1 = " hitesh ";
+console.log(newString1);
+console.log(newString1.trim());
+
+const url = "https://hitesh.com/hitesh%20chaudhary";
+
+console.log(url.replace('%20', '-'))
+
+console.log(url.includes('hitesh'));
+
+console.log(gameName.split('-'));
--- 01_basics/dataTypesSummary.js
+++ 01_basics/dataTypesSummary.js
@@ -12,7 +12,7 @@ let userEmail;
 const id = Symbol('123')
 const anotherID = Symbol('123')
 
-console.log(id === anotherID)
+// console.log(id === anotherID)
 
 const bigNumber = 1245467453478698709089n
 
@@ -28,8 +28,34 @@ let myObj = {
     age: 22,
 }
 
-const myFunction = function(){
-    console.log("Hello world");
+// const myFunction = function(){
+//     console.log("Hello world");
+// }
+
+// console.log(typeof myFunction);
+
+
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// Stack (Primitive) => pass by value, Heap(Non-Primitive) => pass by reference
+
+let myYoutubeName = "riteshshami@gmail.com";
+
+let anotherName = myYoutubeName;
+anotherName = "chaiAurCode";
+
+console.log(anotherName);
+// console.log(myYoutubeName);
+
+let user1 = {
+    email: "user@google.com",
+    user: "Ritesh",
 }
 
-console.log(typeof myFunction);
+let user2 = user1;
+
+user2.email = "riteshshami@gmail.com"
+
+console.log(user2.email);
+console.log(user1.email);
