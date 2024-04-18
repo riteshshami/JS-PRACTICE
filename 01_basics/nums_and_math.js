1d82c9f98adf09ac6a63e8b0ad3adfee8a4864c7
Ritesh <riteshshami@gmail.com>
2024-04-18 22:30:50 +0530

    adding commits

--- 01_basics/nums_and_math.js
+++ 01_basics/nums_and_math.js
@@ -26,10 +26,10 @@ const hundreds = 1000000
 // console.log(Math.min(4, 3, 6, 8));
 // console.log(Math.max(4, 3, 6, 8));
 
-console.log(Math.random());
-console.log(Math.round(Math.random()*10) + 1);
+// console.log(Math.random());
+// console.log(Math.round(Math.random()*10) + 1);
 
 const min = 10;
 const max = 20;
 
-console.log(Math.floor(Math.random() * (max - min + 1)) + min);
+// console.log(Math.floor(Math.random() * (max - min + 1)) + min);
