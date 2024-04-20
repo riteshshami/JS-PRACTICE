83710b5a7b2ee7b64661b9123186247e54fc6e30
Ritesh <riteshshami@gmail.com>
2024-04-20 09:53:21 +0530

    3 basics completed

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,72 @@
+// console.log("H");
+// console.log("H");
+// console.log("H");
+// console.log("H");
+// console.log("H");
+
+function sayMyName() {
+  console.log("H");
+  console.log("I");
+  console.log("T");
+  console.log("E");
+  console.log("S");
+  console.log("H");
+}
+
+// sayMyName()
+
+// function addTwoNumbers(num1, num2){
+//     console.log(num1 + num2);
+// }
+
+function addTwoNumbers(num1, num2){
+    // let result = num1 + num2;
+    // return result;
+
+    return num1 + num2;
+}
+
+const result = addTwoNumbers(2, 3);
+
+// console.log(result);
+
+function loginUserMessage(userName="Sam"){
+    if(!userName){
+        console.log("Please enter a username");   
+        return      
+    }
+    return `${userName} just logged in`
+}
+
+// console.log(loginUserMessage());
+
+
+
+function calculateCartPrice(...num1){
+    return num1;
+}
+
+function calculateCartPrice2(val1, val2, ...num1){
+    return val1, val2;
+}
+
+// console.log(calculateCartPrice2(200, 400, 500, 600, 700, 800));
+
+const user = {
+    username: "Ritesh",
+    price: 199
+}
+
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+}
+
+handleObject(user);
+
+const myNewArr = [200, 400, 100, 600];
+
+function returnSecondValue(getArray){
+    return getArray[1];
+}
+
+console.log(returnSecondValue(myNewArr));
---
+++ 03_basics/04_IIFE.js
@@ -0,0 +1,15 @@
+// Immedialtely invoked function expression
+
+(function chai(){
+    // named iife
+    console.log('DB CONNECTED');
+})(); // <- semi colon is important
+
+((name) => {
+    // unnamed iife
+    console.log(`DB CONNECTED AGAIN ${name}`)
+})('ritesh')
+
+// global scope ke pollution ko avoid karne ke liye IIFE ka use karo
+
+//  https://witeboard.com/b4f05e30-fec8-11ee-85b2-f58c99310d4a  -> Whiteboard link of behind the scene
