e30a136b8d7003bf2ca2f94cef628a6715e00a7c
Ritesh <riteshshami@gmail.com>
2024-04-24 16:37:53 +0530

    Minor changes

---
+++ 05_iterations/04_.js
@@ -0,0 +1,19 @@
+const myObject = {
+    js: 'JavaScript',
+    cpp: 'C++',
+    rb: 'Swift',
+    swift: 'swift by apple'
+}
+
+// for(const key in myObject){
+//     console.log(`${key} shortcut for ${myObject[key]}`);
+// }
+
+const programming = ["js", "cpp", "py", "c", "java"];
+
+// for(const key in programming){
+//     console.log(key);
+// }
+
+// ---> We cannot iterate over map
+
---
+++ 05_iterations/06_.js
@@ -0,0 +1,26 @@
+// const coding = ["js", "ruby", "java", "python"];
+
+// conat values = coding.forEach((item) => {
+//     console.log(item);
+//     return item
+// })
+
+// console.log(values);
+
+const myNums = [1, 2,3, 45, 6, 7, 8];
+
+// const newNums = myNums.filter((num) => {return num > 4})
+
+const newNums = [];
+
+// myNums.forEach( (num) => {
+//     if (num > 4){
+//         newNums.push(num)
+//     }
+// } )
+
+console.log(newNums);
+
+
+// filter, map, reduce
+
