0e9c15057d69d230d3c5192d933b44e7a79bdd35
Ritesh <riteshshami@gmail.com>
2024-05-03 09:16:15 +0530

    revision continues

--- 01_basics/03_conversionsOperations.js
+++ 01_basics/03_conversionsOperations.js
@@ -3,7 +3,7 @@ let score = true;
 // console.log(typeof score);
 // console.log(typeof(score));
 
-let valueInNumber = Number(score);
+// let valueInNumber = Number(score);
 
 // console.log(valueInNumber);
 
@@ -24,7 +24,7 @@ let booleanIsLoggedIn = Boolean(isLoggedIn);
 let sumNumber = 33;
 
 let stringNumber = Number(isLoggedIn);
-// console.log(stringNumber);
+console.log(stringNumber);
 
 // *************************************************************** Operations ***************************************************************
 
@@ -38,13 +38,13 @@ let str2 = " ritesh";
 let str3 = str1 + str2;
 // console.log(str3);
 
-// console.log("1" + 2);
-// console.log(1 + "2");
-// console.log("1" + 2 + 2);
-// console.log(1 + 2 + "2");
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
 
-// console.log(+true);
-// console.log(+"");
+console.log(+true);
+console.log(+"");
 
 let num1, num2, num3
 
@@ -52,7 +52,7 @@ num1 = num2 = num3 = 2 + 2
 
 let gameCounter = 100
 ++gameCounter;
-// console.log(gameCounter);
+console.log(gameCounter);
 
-// console.log(typeof NaN);
+console.log(typeof NaN);
 
---
+++ 01_basics/tempCodeRunnerFile.js
@@ -0,0 +1,9 @@
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+
+console.log(+true);
+console.log(+"");
